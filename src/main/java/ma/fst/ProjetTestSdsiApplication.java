56f01c067e8292cf94455ede45d6cea345a391f6
taslimamindia <mamadoutaslima.diallo@usmba.ac.ma>
2022-03-07 12:26:58 +0100

    First commit.

---
+++ src/main/java/ma/fst/ProjetTestSdsiApplication.java
@@ -0,0 +1,13 @@
+package ma.fst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTestSdsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetTestSdsiApplication.class, args);
+	}
+
+}
